eef51c41f1e0015207c009908d0db2acac30a774
MervaM01~ <www.manix7@gmail.com>
2023-07-03 18:45:56 +0300

    create js folder

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let nameAcademy = 'Beetroot';
+let nameCourse = 'Front - End';
+let ourCountry = 'Ukraine';
+const studentName = 'Maryana';
+let devSalary = 'Everything depends on knowledge';
+
+console.log(nameAcademy);
+console.log(nameCourse);
+console.log(ourCountry);
+console.log(studentName);
+console.log(devSalary);
+
